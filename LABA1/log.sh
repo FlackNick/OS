1873bc67e5464666653fd2c12c035d84a2e48e92
Никита Чикалёв <nikitacikalev@MacBook-Pro-Nikita.local>
2020-09-28 16:15:36 +0400

    all is ready

--- LABA1/log.sh
+++ LABA1/log.sh
@@ -12,19 +12,19 @@ function check_correctness(){
 }
 
 function DO(){
-    ref="/var/log/anaconda/X.log"
+    ref="/Users/nikitacikalev/Documents/OS/LABA1/X.log"
     while read line
     do
       if [[ "$line" = *"(WW)"* && "$line" != *"(WW) warning, (EE) error, (NI) not implemented, (??) unknown."* ]]
       then
-        echo -e "${line//"(WW)"/"\031[33mWarning:\033[0m"}"
+        echo -e "${line//"(WW)"/"\033[31mWarning:\033[0m"}"
       fi
     done < "$ref"
     while read line
     do
       if [[ "$line" = *"(II)"* && "$line" != *"(++) from command line, (!!) notice, (II) informational,"* ]]
       then
-        echo -e "${line//"(II)"/"\034[94mInformation:\033[0m"}"
+        echo -e "${line//"(II)"/"\033[34mInformation:\033[0m"}"
       fi
     done < "$ref"
 }
